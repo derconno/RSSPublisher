4d7e2d5d33b279f64d22155ba553e72ea038475c
der_conno <der_conno@gmx.de>
2019-01-27 17:31:49 +0100

    added LICENSE

--- RSSPublisher.py
+++ RSSPublisher.py
@@ -1,8 +1,34 @@
+##
+## Copyright (c) 2019 Constantin Schwarz.
+## 
+## This file is part of RSSPublisher 
+## (see https://github.com/derconno/RSSPublisher).
+## 
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program. If not, see <http://www.gnu.org/licenses/>.
+##
 import RSSServer
 
 server = RSSServer.RSSServer()
 
 try:
+    print("""
+    RSSPublisher  Copyright (C) 2019  Constantin Schwarz
+
+    This program comes with ABSOLUTELY NO WARRANTY.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; see LICENSE for details.\n
+    """)
     print('Server started on default port')
     server.start_server()
 except KeyboardInterrupt:
